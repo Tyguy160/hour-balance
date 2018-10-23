c9b5387c99ce4d641edcfcc2ce98eaa3493fa4f0
Tyler Maschino <Tylermaschino@gmail.com>
2018-10-22 20:49:28 -0400

    Added new styling and storing of wakeup/bedtime in state

--- src/components/day.js
+++ src/components/day.js
@@ -1,48 +1,116 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
 import TimePicker from "rc-time-picker";
 import TaskSet from "./taskSet";
 import "../styles/day.css";
 import "rc-time-picker/assets/index.css";
 
-const Day = props => {
-  return (
-    <div className="dayComponent">
-      <div className="dayTitle">{props.day}</div>
-      <div className="tasks">
-        <div className="sleepInput">
-          <span>Wakeup Time</span>
-          <TimePicker
-            showSecond={false}
-            defaultValue={moment()
-              .hour(0)
-              .minute(0)}
-            className="timePicker"
-            format={"h:mm a"}
-            use12Hours
-            inputReadOnly
-          />
+class Day extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      day: props.day,
+      wakeupTime: moment()
+        .hour(6)
+        .minute(0),
+      tasks: [
+        {
+          taskName: "",
+          taskTime: "",
+          taskTimeUnit: ""
+        }
+      ],
+      bedTime: moment()
+        .hour(22)
+        .minute(0)
+    };
+
+    this.addTask = this.addTask.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+    this.updateSleeps = this.updateSleeps.bind(this);
+  }
+
+  addTask(e) {
+    this.setState(
+      {
+        tasks: [
+          ...this.state.tasks,
+          {
+            taskName: "",
+            taskTime: "",
+            taskTimeUnit: ""
+          }
+        ]
+      },
+      () => console.log("Added new task")
+    );
+  }
+
+  updateTask(e, TaskID) {
+    console.log(e, TaskID);
+  }
+
+  // Updates the wake up or bed time based
+  // on the type in the function call
+  updateSleeps(e, type) {
+    let hr = e._d.getHours();
+    let min = e._d.getMinutes();
+    this.setState({
+      [`${type}`]: moment()
+        .hour(hr)
+        .minute(min)
+    });
+  }
+
+  render() {
+    return (
+      <div className="dayComponent">
+        <div className="dayTitle">{this.props.day}</div>
+        {/* <button className="addTaskButton" onClick={this.addTask}>
+          +
+        </button> */}
+        <hr />
+        <div className="tasks">
+          <div className="sleepInput">
+            <span>Wakeup Time</span>
+            <TimePicker
+              showSecond={false}
+              defaultValue={this.state.wakeupTime}
+              className="timePicker"
+              format={"h:mm a"}
+              use12Hours
+              inputReadOnly
+              onChange={e => this.updateSleeps(e, "wakeupTime")}
+            />
+          </div>
+          {this.state.tasks.map((task, index) => (
+            <TaskSet
+              key={index}
+              taskName={task.taskName}
+              taskTime={task.taskTime}
+              taskTimeUnit={task.taskTimeUnit}
+              addTask={this.addTask}
+            />
+          ))}
+          <div className="sleepInput">
+            Bedtime
+            <TimePicker
+              showSecond={false}
+              defaultValue={this.state.bedTime}
+              className="timePicker"
+              format={"h:mm a"}
+              use12Hours
+              inputReadOnly
+              onChange={e => this.updateSleeps(e, "bedTime")}
+            />
+          </div>
         </div>
-        <TaskSet />
-        <TaskSet />
-        <TaskSet />
-        <div className="sleepInput">
-          Bedtime
-          <TimePicker
-            showSecond={false}
-            defaultValue={moment()
-              .hour(0)
-              .minute(0)}
-            className="timePicker"
-            format={"h:mm a"}
-            use12Hours
-            inputReadOnly
-          />
-        </div>
+        <hr />
+        <div className="balance">{`24 - ${8} = ${24 - 8}`}</div>
       </div>
-      <button>+</button>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Day;
--- src/components/taskSet.js
+++ src/components/taskSet.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "../styles/taskSet.css";
-const TaskSet = () => {
+
+// Select all text that has been entered
+function selectText(e) {
+  e.target.select();
+}
+
+const TaskSet = props => {
   return (
     <form className="taskSet">
-      <input type="textbox" placeholder="Enter your task..." />
-      <input type="textbox" placeholder="Time taken" />
+      <input
+        type="textbox"
+        placeholder="Enter your task..."
+        className="taskInput input"
+        onFocus={e => selectText(e)}
+        onKeyPress={e => {
+          if (e.which === 13) {
+            e.preventDefault();
+            props.addTask(e);
+          }
+        }}
+        // value={props.taskName}
+      />
+      <div className="timeInputContainer">
+        <input
+          type="textbox"
+          placeholder="Time"
+          className="timeInput input"
+          onFocus={e => selectText(e)}
+          onKeyPress={e => {
+            if (e.which === 13) {
+              e.preventDefault();
+              props.addTask(e);
+            }
+          }}
+          // value={props.taskTime + " " + props.taskTimeUnit}
+        />
+        <select className="timeUnit">
+          <option value="min">mins</option>
+          <option value="hour">hrs</option>
+        </select>
+      </div>
     </form>
   );
 };
